bbb8a36520cab748ebde4dae47ff83bfc8537fce
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-05-31 10:57:15 +0800

    test:  add test support pgoutput message builder

---
+++ test/support/builders.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+module Builders
+  module_function
+
+  def u8(value) = [value].pack("C")
+  def u16(value) = [value].pack("n")
+  def u32(value) = [value].pack("N")
+  def i32(value) = [value & 0xffff_ffff].pack("N")
+  def u64(value) = [value].pack("Q>")
+  def cstr(value) = "#{value}\0".b
+
+  def begin_msg(lsn: 123, timestamp: 456, xid: 789)
+    "B".b + u64(lsn) + u64(timestamp) + u32(xid)
+  end
+
+  def relation_msg
+    "R".b +
+      u32(42) +
+      cstr("public") +
+      cstr("users") +
+      u8(100) +
+      u16(3) +
+      u8(1) + cstr("id") + u32(23) + i32(-1) +
+      u8(0) + cstr("name") + u32(25) + i32(-1) +
+      u8(0) + cstr("active") + u32(16) + i32(-1)
+  end
+
+  def tuple_values(id:, name:, active: "t")
+    u16(3) +
+      text_value(id.to_s) +
+      text_value(name) +
+      text_value(active)
+  end
+
+  def key_tuple(id:)
+    u16(3) +
+      text_value(id.to_s) +
+      null_value +
+      unchanged_toast_value
+  end
+
+  def binary_tuple
+    u16(3) +
+      binary_value([7].pack("N")) +
+      text_value("Alice") +
+      null_value
+  end
+
+  def insert_msg
+    "I".b + u32(42) + "N".b + tuple_values(id: 7, name: "Alice")
+  end
+
+  def insert_binary_msg
+    "I".b + u32(42) + "N".b + binary_tuple
+  end
+
+  def update_msg_with_old_key
+    "U".b + u32(42) + "K".b + key_tuple(id: 7) + "N".b + tuple_values(id: 7, name: "Bob")
+  end
+
+  def update_msg_with_old_tuple
+    "U".b + u32(42) + "O".b + tuple_values(id: 7, name: "Alice") + "N".b + tuple_values(id: 7, name: "Bob")
+  end
+
+  def update_msg_new_only
+    "U".b + u32(42) + "N".b + tuple_values(id: 7, name: "Bob")
+  end
+
+  def delete_msg_with_key
+    "D".b + u32(42) + "K".b + key_tuple(id: 7)
+  end
+
+  def delete_msg_with_old_tuple
+    "D".b + u32(42) + "O".b + tuple_values(id: 7, name: "Alice")
+  end
+
+  def commit_msg
+    "C".b + u8(0) + u64(10) + u64(11) + u64(12)
+  end
+
+  def text_value(value)
+    value = value.b
+    "t".b + i32(value.bytesize) + value
+  end
+
+  def binary_value(value)
+    value = value.b
+    "b".b + i32(value.bytesize) + value
+  end
+
+  def null_value = "n".b
+
+  def unchanged_toast_value = "u".b
+end
